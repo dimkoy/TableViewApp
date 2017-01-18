1ba85a048b967823055566bf06c67d45c473c5b7
dimkoy <dimkoy01@gmail.com>
2017-01-18 09:24:09 +0400

    create table view

---
+++ tableViewApplication/MyRestrauntTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MyRestrauntTableViewCell.swift
+//  tableViewApplication
+//
+//  Created by Dmitriy on 17/01/2017.
+//  Copyright © 2017 Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class MyRestrauntTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
