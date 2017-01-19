ad21d7699d511d623d64d25e98f2e0b89dda76a1
dimkoy <dimkoy01@gmail.com>
2017-01-19 09:30:30 +0400

    adding action menu for restaurant

--- tableViewApplication/MyRestrauntsTableViewController.swift
+++ tableViewApplication/MyRestrauntsTableViewController.swift
@@ -13,6 +13,31 @@ class MyRestrauntsTableViewController: UITableViewController {
     var restaurantNames = ["Ogonek Grill&Bar", "Ely", "Bonsai", "Dastarhan", "IndiaChina", "X.O", "Balkan grill", "Respublica", "Speak Easy", "Morris Pub", "Tasty stories", "Classic", "Love&Life", "Shock", "Bochka"]
     
     var restaurantImages = ["ogonek.jpg", "elu.jpg", "bonsai.jpg", "dastarhan.jpg", "indokitay.jpg", "x.o.jpg", "balkan.jpg", "respublika.jpg", "speakeasy.jpg", "morris.jpg", "istorii.jpg", "klassik.jpg", "love.jpg", "shok.jpg", "bochka.jpg"]
+    var restaurantType = ["ресторан", "ресторан", "ресторан", "ресторан", "ресторан", "ресторан-клуб", "ресторан", "ресторан", "ресторанный комплекс", "ресторан", "ресторан", "ресторан", "ресторан", "ресторан", "ресторан", "ресторан", ]
+    var restaurantLocation = ["Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа", "Уфа"]
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //create alert controller
+        let actionMenu = UIAlertController(title: nil, message: "What to do?", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        //create action for controller
+        let cancelAction = UIAlertAction(title:"Cancel", style: .cancel, handler: nil)
+        actionMenu.addAction(cancelAction)
+        
+        let callActionHandler = {(action: UIAlertAction!) -> Void in
+            print("CallActionHandler is on")
+            let warningMessage = UIAlertController(title: "Сервис не доступен", message: "В данный момент вызов не может быть выполнен", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            warningMessage.addAction(okAction)
+            self.present(warningMessage, animated: true, completion: nil)
+        }
+        
+        let callAction = UIAlertAction(title: "Call 785765", style: .default, handler: callActionHandler)
+        actionMenu.addAction(callAction)
+    
+        //present controller
+        self.present(actionMenu, animated: true, completion: nil)
+    }
 
 
     override func viewDidLoad() {
@@ -49,6 +74,8 @@ class MyRestrauntsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MyRestrauntTableViewCell
         
         cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.locationLabel.text = restaurantLocation[indexPath.row]
+        cell.typeLabel.text = restaurantType[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
         cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.size.height/2
         cell.thumbnailImageView.clipsToBounds = true
